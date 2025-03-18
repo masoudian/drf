a2d26261dbca6377132d9f5823ee96299bee0a38
masoudian <masoudian@users.noreply.github.com>
2025-03-18 19:12:33 +0100

    messing arount with GET data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,21 @@
+import json
 from django.http import JsonResponse
 
 
-def api_home(request, *args, **kwargs):
-    return JsonResponse({'message': 'django api response here!'})
+def api_home(request, *args, **kwargs): # this is django HttpRequest
+    # request.body
+
+    print(request.GET)
+    body = request.body  # a bite string of json data -- need to convert it to json
+    data = {}
+    try:
+        data = json.loads(body) # string of json data => python dict
+    except:
+        pass
+
+    print(data)
+    data['params'] = request.GET
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={'abc': 123}, json={'query': 'hello world'}) # http request
+
 print(get_response.json())
 print(get_response.status_code)
