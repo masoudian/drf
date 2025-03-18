9bbd6b6d1ae15fc3e85c1c4d4bb1846537624770
masoudian <masoudian@users.noreply.github.com>
2025-03-18 18:26:33 +0100

    create first api and handling it with py_client

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint)
-print(get_response.text)
+print(get_response.json())
 print(get_response.status_code)
