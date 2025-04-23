fc408b4cc7537f453dc21a5c0333a03065623058
masoudian <masoudian@users.noreply.github.com>
2025-04-23 18:54:11 +0200

    moving authentication and permissions to default rest_framework settings

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,6 @@
 from rest_framework import authentication, generics, permissions
 
-from api.Authentication import *
+from api.authentication import *
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -10,11 +10,11 @@ from .permissions import IsStaffPermission
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        TokenAuthentication # for header as 'Bearer'
-        # authentication.TokenAuthentication   # for header as default ('Token')
-    ]
+    # authentication_classes = [  ### don't need it here anymore as we have it in settings now
+    #     authentication.SessionAuthentication,
+    #     TokenAuthentication # for header as 'Bearer'
+    #     # authentication.TokenAuthentication   # for header as default ('Token')
+    # ]
     permission_classes = [permissions.IsAdminUser, IsStaffPermission]
 
     def perform_create(self, serializer):
@@ -30,12 +30,14 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
+    permission_classes = [permissions.IsAdminUser, IsStaffPermission]
 
 
 class ProductUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAdminUser, IsStaffPermission]
 
     def perform_update(self, serializer):
         instance = serializer.save()
@@ -47,6 +49,7 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAdminUser, IsStaffPermission]
 
     def perform_destroy(self, instance):
         ## instance ...
